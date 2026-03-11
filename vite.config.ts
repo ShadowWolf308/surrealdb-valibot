90a0adac5c04d21ce36824dcc80a5e7ad29c5289
Levy van der Valk <levy@yaacomm.com>
2026-03-11 10:17:12 +0100

    change: updated bundling logic and moved from pnpm to bun

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig((env) => ({
 	publicDir: env.command === "serve" ? "public" : undefined,
 	build: {
 		lib: {
 			entry: "./lib/index.ts",
+			formats: ["es", "cjs"],
 			name: "surrealdbValibot",
 			fileName: "surrealdbValibot",
 		},
 		rollupOptions: {
-			external: [
-				"valibot",
-				"surrealdb",
-			],
+			external: ["valibot", "surrealdb"],
 			output: {
 				globals: {
-					'valibot': 'valibot',
-					'surrealdb': 'surrealdb',
+					valibot: "valibot",
+					surrealdb: "surrealdb",
 				},
 			},
 		},
 	},
+	plugins: [dts()],
 }));
